031fcb45583af08775762fa52898563d5c4da0a4
jundaehee0 <jundaehee0@naver.com>
2024-06-10 17:33:11 +0900

    feat: 수입&지출 폼 구현

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
-<template lang="">
-  <div>
-    테스트테스트
+<template>
+  <div id="app">
+    <!-- <router-view></router-view> -->
+    <button @click="modalOpen">Modal</button>
+    <!-- {{ modalCheck }} -->
+    <IncomeExpenseForm  :showModal="modalCheck" @form-submitted="handleSubmit" />
   </div>
 </template>
-<script>
-export default {
-  name: "App"
-}
+
+<script setup>
+import { ref } from 'vue';
+import IncomeExpenseForm from './components/IncomeExpenseForm.vue';
+
+const modalCheck = ref(false);
+
+const modalOpen = () => {
+  modalCheck.value = true;
+};
+
+const handleSubmit = (data) => {
+  console.log('Form submitted:', data);
+};
 </script>
-<style lang="">
-  
+
+<style>
+/* 스타일링 */
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from '../App.vue';
+import IncomeExpenseForm from "../components/IncomeExpenseForm.vue";
 
 // import Products from "@/components/Products.vue";
 // import Login from "@/components/Login.vue";
@@ -6,20 +8,37 @@ import { createRouter, createWebHistory } from "vue-router";
 // import Home from "@/components/Home.vue";
 // import Product from "@/components/Product.vue";
 
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/income-expense-form',
+    name: 'IncomeExpenseForm',
+    component: IncomeExpenseForm
+  }
+];
+
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    // { path: "/", component: Home },
-    // // { path: "/products", component: Products },
-    // {
-    //   path: "/products", // 중첩라우팅
-    //   component: Products, 
-    //   children: [{ name: "Product", path: "/product/:prodNo", component: Product }],
-    // },
-    // { path: "/login", component: Login },
-    // { path: "/signup", component: Signup },
-    // // { name: "Product", path: "/product/:prodNo", component: Product },
-  ],
+  history: createWebHistory('/my-app.vue'),
+  routes
 });
+// const router = createRouter({
+//   history: createWebHistory(process.env.BASE_URL),
+//   routes: [
+//     // { path: "/", component: Home },
+//     // // { path: "/products", component: Products },
+//     // {
+//     //   path: "/products", // 중첩라우팅
+//     //   component: Products, 
+//     //   children: [{ name: "Product", path: "/product/:prodNo", component: Product }],
+//     // },
+//     // { path: "/login", component: Login },
+//     // { path: "/signup", component: Signup },
+//     // // { name: "Product", path: "/product/:prodNo", component: Product },
+//   ],
+// });
 
 export default router;
