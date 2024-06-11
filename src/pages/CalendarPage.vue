1aaa6181585664bb619894a00bd185df4eb88642
Doyeon <duseh729@naver.com>
2024-06-11 11:01:57 +0900

    feat: Calender 오른쪽 (자산상태, 메모폼) 구현

--- src/pages/CalendarPage.vue
+++ src/pages/CalendarPage.vue
@@ -1,12 +1,20 @@
 <!-- 달력 페이지 -->
 <template>
   <div style="padding: 50px">
-    <Calendar />
+    <div style="display: flex">
+      <Calendar />
+      <div>
+        <AssetStatus />
+        <MemoForm />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import Calendar from "../components/Calendar/Calendar.vue";
+import AssetStatus from "../components/Calendar/AssetStatus.vue";
+import MemoForm from "../components/Calendar/MemoForm.vue";
 </script>
 
 <style scoped></style>
