88e3631d843343e3e046e949237bc3eef236e85d
Doyeon <duseh729@naver.com>
2024-06-12 09:47:48 +0900

    feat: 달력에 수입, 지출 숫자 디스플레이

--- src/components/Calendar/MemoForm.vue
+++ src/components/Calendar/MemoForm.vue
@@ -2,12 +2,26 @@
   <div class="memo-container">
     <h4>메모</h4>
     <form action="">
-      <label for="input-memo">6월 10일 (월요일)</label>
+      <label for="input-memo">{{`${year}년 ${month+1}월 ${date}일`}}</label>
       <input type="text" placeholder="텍스트를 입력하세요." name="input-memo">
     </form>
   </div>
 </template>
 <script setup>
+const props = defineProps({
+  year: {
+    type: Number,
+    required: true
+  },
+  month: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Number,
+    required: true
+  },
+});
 </script>
 <style >
   @import url('../../assets/css/Calendar/memo-form.css');
