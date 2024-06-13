f26a4fc7fa9f8d56332047be649e3ba0273c0a24
Doyeon <duseh729@naver.com>
2024-06-13 11:22:08 +0900

    refactor: 달력페이지 메인으로 변경

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,6 @@
       <div class="flex-default">
         <ul class="flex-default">
           <router-link to="/history" style="text-decoration: none"><li ref="history" class="nav-item cursor-pointer">내역</li></router-link>
-          <router-link to="/calendar" style="text-decoration: none"><li ref="calendar" class="nav-item cursor-pointer">달력</li></router-link>
           <router-link to="/asset" style="text-decoration: none"><li ref="asset" class="nav-item cursor-pointer">자산</li></router-link>
           <router-link to="/statistics" style="text-decoration: none"><li ref="statistics" class="nav-item cursor-pointer">통계</li></router-link>
         </ul>
@@ -21,7 +20,6 @@ import { useRoute } from "vue-router";
 const route = useRoute();
 
 const history = ref(null);
-const calendar = ref(null);
 const asset = ref(null);
 const statistics = ref(null);
 
@@ -29,15 +27,12 @@ const updateStyles = () => {
   // 초기화
   // console.log(history.value)
   normalStyle(history.value);
-  normalStyle(calendar.value);
   normalStyle(asset.value);
   normalStyle(statistics.value);
 
   // 현재 경로에 따라 스타일 적용
   if (route.fullPath.includes("history")) {
     visitedStyle(history.value);
-  } else if (route.fullPath.includes("calendar")) {
-    visitedStyle(calendar.value);
   } else if (route.fullPath.includes("asset")) {
     visitedStyle(asset.value);
   } else if (route.fullPath.includes("statistics")) {
