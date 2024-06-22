33b65468a4a3b2f95c3ce4589b9c02fed2ccb94b
Doyeon <duseh729@naver.com>
2024-06-22 17:16:40 +0900

    다른 경로 요청 시 /로 보내버림

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import App from '../App.vue';
-import IncomeExpenseForm from '../components/IncomeExpenseForm.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import App from "../App.vue";
+import IncomeExpenseForm from "../components/IncomeExpenseForm.vue";
 
-import History from '../pages/History.vue';
-import CalendarPage from '../pages/CalendarPage.vue';
-import Asset from '../pages/Asset.vue';
-import Statistics from '../pages/Statistics.vue';
+import History from "../pages/History.vue";
+import CalendarPage from "../pages/CalendarPage.vue";
+import Asset from "../pages/Asset.vue";
+import Statistics from "../pages/Statistics.vue";
 
 const router = createRouter({
-    history: createWebHistory('/my-app.vue'),
-    routes: [
-        { path: '/',  component: CalendarPage },
-        { path: '/history', component: History },
-        { path: '/asset', component: Asset },
-        { path: '/statistics', component: Statistics },
-    ],
+  history: createWebHistory("/my-app.vue"),
+  routes: [
+    { path: "/", component: CalendarPage },
+    { path: "/history", component: History },
+    { path: "/asset", component: Asset },
+    { path: "/statistics", component: Statistics },
+    // 다른 라우트들
+    { path: "*", redirect: "/" },
+  ],
 });
 
 export default router;
